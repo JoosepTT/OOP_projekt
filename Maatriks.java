2f75b549fc0f6adb95b5ff3d99bf3973c30c2b7f
LauriLaud <61418385+LauriLaud@users.noreply.github.com>
2025-03-25 13:16:02 +0200

    Add files via upload

---
+++ Maatriks.java
@@ -0,0 +1,137 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public abstract class Maatriks {
+    protected int size;
+    protected int[][] andmeMaatriks;
+    protected char[][] ekraaniMaatriks;
+    protected int sundmusteProtsent;
+    protected static int asukohtX;
+    protected static int asukohtY;
+    protected int skoor = 0;
+    protected int elud = 3;
+    private static final char mängija = 'M';
+    private static final char käidud = 'X';
+
+    public Maatriks(int size, int sundmusteProtsent) {
+        this.size = size;
+        this.sundmusteProtsent = sundmusteProtsent;
+        this.andmeMaatriks = new int[size][size];
+        this.ekraaniMaatriks = new char[size][size];
+        looMaatriks();
+    }
+
+    // maatriksi loomine (avaliku ja salajase)
+    public void looMaatriks() {
+        Random suvaline = new Random();
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                andmeMaatriks[i][j] = (suvaline.nextInt(100) < sundmusteProtsent) ? 1 : 0;
+                ekraaniMaatriks[i][j] = 'O';
+            }
+        }
+
+        asukohtX = suvaline.nextInt(size);
+        asukohtY = suvaline.nextInt(size);
+        ekraaniMaatriks[asukohtX][asukohtY] = 'M';
+
+    }
+
+    // kasutajale maatiksi kuvamine
+    public void kuvaMaatriks() {
+        for (char[] rida : ekraaniMaatriks) {
+            for (char element : rida) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void kuvaMaatriks2() {
+        for (int[] rida : andmeMaatriks) {
+            for (int element : rida) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // mängutsükkel
+    public void manguTsukkel(Scanner scanner) {
+        while (elud > 0) {
+            System.out.println("Hetkeskoor: " + skoor + "  Elud: " + "| ".repeat(elud));
+            kuvaMaatriks();
+            System.out.println();
+            kuvaMaatriks2();
+
+            int eventNum = new Random().nextInt(3) + 1;
+            executeEvent(eventNum);
+
+            int sihtmark = new Random().nextInt(16) + 3;
+            System.out.println("Sinu sihtmärk: " + sihtmark);
+
+            System.out.println("Kuhu soovid liikuda? (w - üles, s - alla, a - vasakule, d - paremale)");
+            char liigu = scanner.next().charAt(0);
+            if (liigu == 'q') {
+                System.out.println("Mäng lõpetatud.");
+                scanner.close();
+                break;
+            } else {
+                liiguta(ekraaniMaatriks, liigu);
+            }
+        }
+        System.out.println("Mäng läbi! Sinu lõppskoor: " + skoor);
+    }
+
+    public static void liiguta(char[][] kaart, char suund) {
+        /**
+         * Kontrollib kas liikumine on sobiv (ei liigu üle kaardi piiride), ja kui kasutaja sisestus
+         * on õige (w,s,a,d) siis liigutab mängijat ühe võrra soovitud suunas.
+         * @param kaart antud kaart.
+         * @param suund kasutaja sisestus (w,s,a,d)
+         */
+        int uusX = asukohtX, uusY = asukohtY;
+
+        switch (suund) {
+            case 'w':
+                uusX--;
+                break;
+            case 's':
+                uusX++;
+                break;
+            case 'a':
+                uusY--;
+                break;
+            case 'd':
+                uusY++;
+                break;
+            default:
+                System.out.println("Vale liikumine, sisesta: W/A/S/D.");
+                return;
+        }
+
+        if (onSobivLiikumine(kaart, uusX, uusY)) {
+            kaart[asukohtX][asukohtY] = käidud;
+            asukohtX = uusX;
+            asukohtY = uusY;
+            kaart[asukohtX][asukohtY] = mängija;
+        } else {
+            System.out.println("Sinna ei saa liikuda!");
+        }
+    }
+
+    public static boolean onSobivLiikumine(char[][] kaart, int x, int y) {
+        /**
+         * Kontrollib kas liikumine on sobiv (ei liigu üle kaardi piiride).
+         * @param kaart antud kaart.
+         * @param x suund vasakult-paremale
+         * @param y suund ülevalt-alla
+         * @return tagastab true, kui liikumine õige ja false, kui liikumine on vale.
+         */
+        return x >= 0 && x < kaart.length && y >= 0 && y < kaart[0].length;
+    }
+
+    public abstract void executeEvent(int eventNum);
+
+}
---
+++ Main.java
@@ -0,0 +1,131 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    private static final String failinimi = "Skoorid.txt";
+    private static Map<String, Integer> skoorid = new HashMap<>();
+
+    public static void main(String[] args) {
+        laadiSkoorid(); // faili sisselugemine
+        Scanner scanner = new Scanner(System.in); // skänneri ülesseadmine
+        algus(); // mängu pealkirja kuvamine
+
+        // valikumenüü kuvamine
+        while (true) {
+            System.out.println("Vali tegevus: 1 - Alusta mängu, 2 - Vaata skooritabelit, 3 - Välju");
+            int action = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (action) {
+                case 1:
+                    alustaMangu(scanner);
+                    break;
+                case 2:
+                    kuvaSkoorid();
+                    break;
+                case 3:
+                    System.out.println("Mäng lõpetatud.");
+                    return;
+                default:
+                    System.out.println("Vale valik, proovi uuesti.");
+            }
+            scanner.close();
+        }
+    }
+
+    // Prindib suurelt mängu pealkirja (pole kujundatud)
+    public static void algus() {
+        System.out.println("<Ardejaht>");
+        System.out.println();
+    }
+
+    // rakustaseme valimine
+    private static void alustaMangu(Scanner scanner) {
+        System.out.println("Sisesta oma nimi:");
+        String mangijaNimi = scanner.nextLine().trim();
+
+        int korgeimSkoor = skoorid.getOrDefault(mangijaNimi, 0);
+        System.out.println("Sinu senine kõrgeim skoor: " + korgeimSkoor);
+
+        int valik;
+        double[] kordajad = {1.0, 1.5, 2.0};
+
+        // raskustaseme valimine
+        do {
+            System.out.println("Vali raskustase: 1 - Kerge, 2 - Keskmine, 3 - Raske");
+            while (!scanner.hasNextInt()) {
+                System.out.println("Palun sisesta vastav number!");
+                scanner.next();
+            }
+            valik = scanner.nextInt();
+        } while (valik < 1 || valik > 3);
+
+        Maatriks mang;
+
+        switch (valik) {
+            case 1:
+                mang = new Raskustase_1(kordajad[0]);
+                break;
+            case 2:
+                mang = new Raskustase_2(kordajad[1]);
+                break;
+            case 3:
+                mang = new Raskustase_3(kordajad[2]);
+                break;
+            default:
+                mang = new Raskustase_1(kordajad[0]); // vaikeväärtus
+        }
+
+        mang.manguTsukkel(scanner);
+
+        /*
+        // skoori kohahoidja
+        int skoor = new Random().nextInt(100);
+        System.out.println("Sinu skoor: " + skoor);
+        uuendaSkoori(mangijaNimi, skoor);
+        salvestaSkoor();
+        */
+    }
+
+    // skooritabeli kuvamine
+    private static void kuvaSkoorid() {
+        System.out.println("Skooritabel:");
+        for (Map.Entry<String, Integer> sissekanne : skoorid.entrySet()) {
+            System.out.println(sissekanne.getKey() + " - " + sissekanne.getValue());
+        }
+    }
+
+    // skooride sisselaadimine
+    private static void laadiSkoorid() {
+        try (BufferedReader br = new BufferedReader(new FileReader(failinimi))) {
+            String rida;
+
+            while ((rida = br.readLine()) != null) {
+                String[] parts = rida.split(" ; ");
+                if (parts.length == 2) {
+                    skoorid.put(parts[0], Integer.parseInt(parts[1]));
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Skoorifaili ei leitud.");
+        }
+    }
+
+    // skoori faili kirjutamine
+    private static void salvestaSkoor() {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(failinimi))) {
+            for (Map.Entry<String, Integer> sisend : skoorid.entrySet()) {
+                bw.write(sisend.getKey() + " ; " + sisend.getValue());
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Tekkis viga skoorifaili salvestamisel.");
+        }
+    }
+
+    // skoori värskendamine
+    private static void uuendaSkoori(String mangijaNimi, int uusSkoor) {
+        skoorid.put(mangijaNimi, Math.max(skoorid.getOrDefault(mangijaNimi, 0), uusSkoor));
+    }
+
+}
