7c52ca5563f8692f4cf0d419c214386ea9de9aac
Joosep Tiger Tilgar <jooseptiger@ut.ee>
2025-04-02 19:48:08 +0300

    Update Raskustase_3.java

--- Raskustase_3.java
+++ Raskustase_3.java
@@ -1,20 +1,55 @@
+
 public class Raskustase_3 extends Maatriks {
-    private double punktiKordaja = 2.0;
+    private Sundmused sundmused = new Sundmused();
 
-    public Raskustase_3(double punktiKordaja) {
-        super(7, 30);
-        this.punktiKordaja = punktiKordaja;
+    public Raskustase_3() {
+        super(7, 70);
     }
 
-    public void rt3() {
-
-    }
-
-    public void executeEvent(int eventNum) {
+    public void executeEvent(int eventNum) throws InterruptedException {
         switch (eventNum) {
-            case 1: System.out.println("Leidsid 15 punkti!"); skoor += 15; break;
-            case 2: System.out.println("Kaotasid 2 elu!"); elud -= 2; break;
-            case 3: System.out.println("Ei juhtunud midagi."); break;
+            case 1:
+                sundmused.sundmus_1(3);
+                break;
+            case 2:
+                sundmused.sundmus_2(3);
+                break;
+            case 3:
+                sundmused.sundmus_3(3);
+                break;
+            case 4:
+                sundmused.sundmus_4(3);
+                break;
+            case 5:
+                sundmused.sundmus_5(3);
+                break;
+            case 6:
+                sundmused.sundmus_6(3);
+                break;
+            case 7:
+                sundmused.sundmus_7(3);
+                break;
+            case 8:
+                sundmused.sundmus_8(3);
+                break;
+            case 9:
+                sundmused.sundmus_9(3);
+                break;
+            case 10:
+                sundmused.sundmus_10(3);
+                break;
+            case 11:
+                sundmused.sundmus_11(3);
+                break;
+            case 12:
+                sundmused.sundmus_12(3);
+                break;
+            case 13:
+                sundmused.sundmus_13(3);
+                break;
+            case 14:
+                sundmused.sundmus_14(3);
+                break;
         }
     }
 }
